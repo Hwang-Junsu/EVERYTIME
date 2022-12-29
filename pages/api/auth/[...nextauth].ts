b1adf83ec59488d75a92436eb468a2da2c6ae515
Hwang-Junsu <hjs77728@gmail.com>
2022-12-30 00:17:08 +0900

    Feat: Social Login

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,58 +2,62 @@ import * as bcrypt from "bcrypt";
 import CredentialsProvider from "next-auth/providers/credentials";
 import NextAuth from "next-auth";
 import client from "@libs/server/client";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import {PrismaAdapter} from "@next-auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
 import KakaoProvider from "next-auth/providers/kakao";
 
 export default NextAuth({
-  providers: [
-    CredentialsProvider({
-      name: "email-password-credential",
-      credentials: {
-        email: { label: "Email", type: "email", placeholder: "test@test.com" },
-        password: { label: "Password", type: "password" },
-      },
+    providers: [
+        CredentialsProvider({
+            name: "email-password-credential",
+            credentials: {
+                email: {
+                    label: "Email",
+                    type: "email",
+                    placeholder: "test@test.com",
+                },
+                password: {label: "Password", type: "password"},
+            },
 
-      async authorize(credentials) {
-        // Add logic here to look up the user from the credentials supplied
-        const user = await client.user.findUnique({
-          where: { email: credentials?.email },
-          select: { name: true, email: true, password: true },
-        });
-        const matchedPassword = await bcrypt.compare(
-          credentials?.password as string,
-          user?.password as string
-        );
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user;
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null;
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-        }
-      },
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-    KakaoProvider({
-      clientId: process.env.KAKAO_CLIENT_ID as string,
-      clientSecret: process.env.KAKAO_CLIENT_SECRET as string,
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, account, profile, isNewUser }) {
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      return token;
+            // typescript bug
+            async authorize(credentials) {
+                const user = await client.user.findUnique({
+                    where: {email: credentials?.email},
+                    select: {name: true, email: true, password: true},
+                });
+                const matchedPassword = await bcrypt.compare(
+                    credentials?.password as string,
+                    user?.password as string
+                );
+                if (!matchedPassword) {
+                    throw new Error("Password Incorrect");
+                }
+                if (user) {
+                    return user;
+                } else {
+                    return null;
+                }
+            },
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        }),
+        KakaoProvider({
+            clientId: process.env.KAKAO_CLIENT_ID as string,
+            clientSecret: process.env.KAKAO_CLIENT_SECRET as string,
+        }),
+    ],
+    callbacks: {
+        async jwt({token, account}) {
+            if (account) {
+                token.accessToken = account.access_token;
+            }
+            return token;
+        },
     },
-  },
-  secret: "123132131fsdvfawefae",
-  adapter: PrismaAdapter(client),
-  pages: { signIn: "/login" },
-  session: { strategy: "jwt" },
+    secret: "123132131fsdvfawefae",
+    adapter: PrismaAdapter(client),
+    pages: {signIn: "/login"},
+    session: {strategy: "jwt"},
 });
--- pages/login.tsx
+++ pages/login.tsx
@@ -2,65 +2,86 @@ import Button from "@components/button";
 import Input from "@components/input";
 import Head from "next/head";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { useForm } from "react-hook-form";
-import { ILoginRequest } from "types/types";
-import { signIn } from "next-auth/react";
+import {useRouter} from "next/router";
+import {useForm} from "react-hook-form";
+import {ILoginRequest} from "types/types";
+import {signIn} from "next-auth/react";
+import Image from "next/image";
+import Swal from "sweetalert2";
 
 export default function Login() {
-  const { register, handleSubmit } = useForm<ILoginRequest>();
-  const router = useRouter();
-  const onValid = async (data: ILoginRequest) => {
-    const response = await signIn("credentials", {
-      ...data,
-      redirect: false,
-    });
-    if (response?.ok) router.push("/");
-  };
-  const onGoogleLogin = async () => {
-    const response = await signIn("google", { redirect: false });
-  };
-  const onKakaoLogin = async () => {
-    const response = await signIn("kakao", { redirect: false });
-  };
+    const {register, handleSubmit} = useForm<ILoginRequest>();
+    const router = useRouter();
+    const onValid = async (data: ILoginRequest) => {
+        const response = await signIn("credentials", {
+            ...data,
+            redirect: false,
+        });
+        if (response?.ok) router.push("/");
+    };
+    const onGoogleLogin = async () => {
+        const response = await signIn("google", {redirect: false});
+        if (response.ok) {
+            router.push("/");
+            return;
+        }
+        Swal.fire("로그인 오류입니다.");
+    };
+    const onKakaoLogin = async () => {
+        const response = await signIn("kakao", {redirect: false});
+        if (response.ok) {
+            router.push("/");
+            return;
+        }
+        Swal.fire("로그인 오류입니다.");
+    };
 
-  return (
-    <>
-      <Head>
-        <title>JUNSTAGRAM</title>
-      </Head>
-      <div className="flex flex-col items-center justify-center h-screen px-4">
-        <form
-          onSubmit={handleSubmit(onValid)}
-          className="flex flex-col w-3/4 p-3 py-16 mx-auto mb-4 space-y-10 border-2 border-solid"
-        >
-          <h1 className="font-bold tracking-[-3px] text-4xl text-center">
-            JUNSTAGRAM
-          </h1>
-          <div className="space-y-5">
-            <Input
-              register={register("email")}
-              type="text"
-              placeholder="이메일"
-            />
-            <Input
-              register={register("password")}
-              type="password"
-              placeholder="비밀번호"
-            />
-          </div>
-          <Button text="로그인" large />
-        </form>
-        <div className="w-3/4 p-3 mx-auto border-2 border-solid">
-          <Link href="/join">
-            <div className="font-bold text-center cursor-pointer hover:underline hover:text-blue-500">
-              계정이 없으신가요?
+    return (
+        <>
+            <Head>
+                <title>JUNSTAGRAM</title>
+            </Head>
+            <div className="flex flex-col items-center justify-center h-screen px-4">
+                <form
+                    onSubmit={handleSubmit(onValid)}
+                    className="flex flex-col w-3/4 p-3 py-16 mx-auto mb-4 space-y-10 border-2 border-solid"
+                >
+                    <h1 className="font-bold tracking-[-3px] text-4xl text-center">
+                        JUNSTAGRAM
+                    </h1>
+                    <div className="space-y-5">
+                        <Input
+                            register={register("email")}
+                            type="text"
+                            placeholder="이메일"
+                        />
+                        <Input
+                            register={register("password")}
+                            type="password"
+                            placeholder="비밀번호"
+                        />
+                    </div>
+                    <Button text="로그인" large />
+                </form>
+                <div className="w-3/4 p-3 mx-auto space-y-2 border-2 border-solid">
+                    <Link href="/join">
+                        <div className="font-bold text-center cursor-pointer hover:underline hover:text-blue-500">
+                            계정이 없으신가요?
+                        </div>
+                    </Link>
+                    <div className="flex flex-col items-center justify-center w-full space-y-2">
+                        <button
+                            onClick={onGoogleLogin}
+                            className="w-[210px] h-[50px] bg-[url('../images/btn_google_signin_light_focus_web.png')] bg-cover bg-no-repeat bg-center shadow-lg hover:scale-105"
+                        />
+
+                        <button
+                            className="w-[210px] h-[50px] bg-[url('../images/kakao_login_medium_narrow.png')] bg-cover bg-no-repeat bg-center shadow-lg hover:scale-105"
+                            onClick={onKakaoLogin}
+                        />
+                    </div>
+                </div>
             </div>
-          </Link>
-          <button onClick={onGoogleLogin}>구글 로그인</button>
-          <button onClick={onKakaoLogin}>카카오 로그인</button>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 }
