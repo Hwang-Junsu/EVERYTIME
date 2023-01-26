8ac33f92580a9f45a722a1b2cd3acdd928d734bb
Hwang-Junsu <hjs77728@gmail.com>
2023-01-27 03:23:52 +0900

    Deploy

--- next.config.js
+++ next.config.js
@@ -14,6 +14,13 @@ const nextConfig = {
     NEXT_PUBLIC_CLOUDFLARE_CLIENT_ID: process.env.CLOUDFLARE_CLIENT_ID,
     NEXT_PUBLIC_CLOUDFLARE_API_TOKEN: process.env.CLOUDFLARE_API_TOKEN,
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
